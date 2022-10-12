137299375c52e315adb3a34c11eb2179a88b6e2c
sleepless <arnas.manucan@gmail.com>
2022-10-12 15:42:27 +0300

    Add props to card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,32 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
 
+
 function App() {
   return (
     <div className='container'>
       <Navbar />
       <Hero />
-      <Card />
+      <div className='card_section'>
+        <Card
+          img='../public/card-img.png'
+          exp='Life lessons with Katie Zaferes'
+          price='From $136'
+          status='SOLD OUT'
+        />
+        <Card
+          img='../public/card-img2.png'
+          exp='Learn wedding photography'
+          price='From $125'
+          status='ONLINE'
+        />
+        <Card
+          img='../public/card-img3.png'
+          exp='Group mountain biking'
+          price='From $50'
+          status='ONLINE'
+        />
+      </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,23 @@
-import cardImg from '../assets/card-img.png'
 import star from '../assets/star.png'
 import './Card.css'
 
-function Card() {
+function Card(props) {
+    const image = props.img
+    const experience = props.exp
+    const price = props.price
+    const status = props.status
+
     return (
         <div className='card'>
-            <div className='status'>SOLD OUT</div>
-            <img src={cardImg} alt='picture of a person' className='card_img'></img>
+            <div className='status'>{status}</div>
+            <img src={image} alt='picture of a person' className='card_img'></img>
             <div className='rating_container'>
                 <img src={star} alt='rating star image' className='star_img'></img>
                 <p className='rating'><b>5.0</b></p>
                 <p className='rating_num'>(6)&bull;USA</p>
             </div>
-            <p className='card_text'>Life lessons with Katie Zaferes</p>
-            <p><b>From $136</b> / person</p>
+            <p className='card_text'>{experience}</p>
+            <p><b>{price}</b> / person</p>
         </div>
     )
 }
