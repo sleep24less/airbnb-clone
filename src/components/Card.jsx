7ea62e505f87b4cc6d75fc19032889b65f744d08
sleepless <arnas.manucan@gmail.com>
2022-10-14 17:19:10 +0300

    Make card jsx cleaner

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,18 @@ import star from '../assets/star.png'
 import './Card.css'
 
 function Card(props) {
-    const image = props.img
-    const experience = props.exp
-    const price = props.price
-    const status = props.status
 
     return (
         <div className='card'>
-            <div className='status'>{status}</div>
-            <img src={image} alt='picture of a person' className='card_img'></img>
+            <div className='status'>{props.status}</div>
+            <img src={props.img} alt='picture of a person' className='card_img'></img>
             <div className='rating_container'>
                 <img src={star} alt='rating star image' className='star_img'></img>
                 <p className='rating'><b>5.0</b></p>
                 <p className='rating_num'>(6)&bull;USA</p>
             </div>
-            <p className='card_text'>{experience}</p>
-            <p><b>{price}</b> / person</p>
+            <p className='card_text'>{props.exp}</p>
+            <p><b>{props.price}</b> / person</p>
         </div>
     )
 }
