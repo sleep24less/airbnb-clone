d06db1c15b7f323ab919c90b9a6729e62e87cbcc
sleepless <arnas.manucan@gmail.com>
2022-10-18 12:01:54 +0300

    Create card components using data array

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,31 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        exp={item.title}
+        price={item.price}
+        status='SOLD OUT'
+        rating={item.stats.rating}
+        count={item.stats.reviewCount}
+        country={item.location}
+      />
+    )
+  })
+
   return (
     <div className='container'>
       <Navbar />
       <Hero />
       <div className='card_section'>
-        <Card
-          img='card-img.png'
-          exp='Life lessons with Katie Zaferes'
-          price='136'
-          status='SOLD OUT'
-          rating={5.0}
-          count={6}
-          country='USA'
-        />
-        <Card
-          img='card-img2.png'
-          exp='Learn wedding photography'
-          price='125'
-          status='ONLINE'
-          rating={4.5}
-          count={10}
-          country='Yemen'
-        />
-        <Card
-          img='card-img3.png'
-          exp='Group mountain biking'
-          price='50'
-          status='ONLINE'
-          rating={3.6}
-          count={3}
-          country='Italy'
-        />
+        {cards}
       </div>
     </div>
   )
